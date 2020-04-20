32d9eaf95702288a469bb1f237a06f8d968efec7
Amir Ramic <amir.ramic@hotmail.com>
2020-04-20 18:02:17 +0200

    Add test api

--- main.js
+++ main.js
@@ -6,4 +6,8 @@ app.get('/', (req, res) => {
     res.send('Hello World')
 });
 
+app.get('/message', (req, res) => {
+    res.send('Hello from heroku server app')
+});
+
 app.listen(PORT, _ => console.log(`Server listening on port... ${PORT}`));
